d5b3b9a28f27e1386bc71cdd4b8d0ee5afa204e2
Murilo Oliveira <murilo.oliveira@pentagro.com.br>
2023-08-24 15:14:26 -0300

    a

--- src/api/update.js
+++ src/api/update.js
@@ -1,2 +1,20 @@
-const test = "a";
-console.log(test)
+const files_to_commit = require("../config/to_commit.json");
+
+if(JSON.stringify(files_to_commit) != "{}"){
+    const date = new Date();
+    
+    let dateFormat = ("0" + date.getDate()).slice(-2);
+    let monthFormat = ("0" + (date.getMonth() + 1)).slice(-2);
+    let hourFormat = ("0" + date.getHours()).slice(-2);
+    let minuteFormat = ("0" + date.getMinutes()).slice(-2);
+    let secondFormat = ("0" + date.getSeconds()).slice(-2);
+
+    const version_str = `${date.getFullYear()}${monthFormat}${dateFormat}${hourFormat}${minuteFormat}${secondFormat}`;
+
+    
+
+    console.log(version_str)
+    console.log(files_to_commit);
+    console.log(typeof files_to_commit);
+    console.log(JSON.stringify(files_to_commit));
+}
