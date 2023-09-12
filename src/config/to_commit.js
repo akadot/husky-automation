3ac36d833aa6d8a74ae8f302de85149e05387a38
Murilo Oliveira <murilo.oliveira@pentagro.com.br>
2023-09-12 11:35:27 -0300

    :tada: Migrating to github

--- src/config/to_commit.js
+++ src/config/to_commit.js
@@ -1,3 +1,6 @@
 let files = [
+'src/Test.Server/fim.js',
+'src/Test.Web/file.js',
+'src/config/update.js',
 ]
 export default files
--- src/config/update.js
+++ src/config/update.js
@@ -13,32 +13,32 @@ if(files != undefined && files != null && files != "" && files.length && files.l
     const version_str = `${date.getFullYear()}-${monthFormat}-${dateFormat}-${hourFormat}-${minuteFormat}-${secondFormat}`;
 
     for(let filename of files){
-        if(!filename.includes("api-version.json"))
+        if(!filename.includes("version.json"))
         {
-            if(filename.includes("/PentagroPGDI.")){
+            if(filename.includes("/Test.")){
                 let splitted = filename.split("/");
-                let idx = splitted.findIndex(item => item.includes("PentagroPGDI."))
+                let idx = splitted.findIndex(item => item.includes("Test."))
                 if(idx != null && idx != undefined){
                     let absolutePath = splitted.slice(0,idx + 1).join("/")
 
-                    if(absolutePath.includes("API")){
-                        fs.readFile(`./${absolutePath}/api-version.json`,'utf8',(err,file) => {
+                    if(absolutePath.includes("Server")){
+                        fs.readFile(`./${absolutePath}/version.json`,'utf8',(err,file) => {
                             if(!err){
                                 let json = JSON.parse(file)
                                 json.version = version_str;
 
-                                fs.writeFile(`./${absolutePath}/api-version.json`, JSON.stringify(json), (err)=>{
+                                fs.writeFile(`./${absolutePath}/version.json`, JSON.stringify(json), (err)=>{
                                     if(!err) console.log(`Versão ${version_str} atualizada em ./${absolutePath}/api-version.json`)
                                 })
                             }
                         })
-                    }else if(absolutePath.includes("App") || absolutePath.includes("Conf")){
-                        fs.readFile(`./${absolutePath}/public/api-version.json`,'utf8',(err,file) => {
+                    }else if(absolutePath.includes("Web")){
+                        fs.readFile(`./${absolutePath}/public/version.json`,'utf8',(err,file) => {
                             if(!err){
                                 let json = JSON.parse(file)
                                 json.version = version_str;
                                 
-                                fs.writeFile(`./${absolutePath}/public/api-version.json`, JSON.stringify(json), (err)=>{
+                                fs.writeFile(`./${absolutePath}/public/version.json`, JSON.stringify(json), (err)=>{
                                     if(!err) console.log(`Versão ${version_str} atualizada em ./${absolutePath}/public/api-version.json`)
                                 })
                             }
